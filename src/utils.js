c608fb6f28209eb948be009ad83db0ad69cf5872
Josh <joshuaadamparker@hotmail.com>
2018-11-26 15:40:36 -0800

    add getUserId helper fn

---
+++ src/utils.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken')
+const APP_SECRET = 'GraphQL-is-aw3some'
+
+// helper function that you’ll call in resolvers that 
+// require authentication
+function getUserId(context) {
+  const Authorization = context.request.get('Authorization')
+  if (Authorization) {
+    const token = Authorization.replace('Bearer ', '')
+    const { userId } = jwt.verify(token, APP_SECRET)
+    return userId
+  }
+
+  throw new Error('Not authenticated')
+}
+
+module.exports = {
+  APP_SECRET,
+  getUserId,
+}
