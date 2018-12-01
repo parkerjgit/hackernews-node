75da5e77281f2297694f75c35512adeb2f83a857
Joshua Parker <joshuaparker@protonmail.com>
2018-11-30 21:35:12 -0800

    add filter to feed query

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -11,37 +11,37 @@ async function signup(parent, args, context, info) {
     const user = await context.db.mutation.createUser({
         data: { ...args, password },
     }, `{ id }`)
-  
-    // 3. generating a JWT which is signed with an APP_SECRET. 
-    // You still need to create this APP_SECRET and also install 
+
+    // 3. generating a JWT which is signed with an APP_SECRET.
+    // You still need to create this APP_SECRET and also install
     // the jwt library that’s used here.
     const token = jwt.sign({ userId: user.id }, APP_SECRET)
-  
+
     // 4. return the token and the new user
     return {
         token,
         user,
     }
 }
-  
+
 async function login(parent, args, context, info) {
-    
-    // 1. using the Prisma binding instance to retrieve the 
+
+    // 1. using the Prisma binding instance to retrieve the
     // existing User record by the email
     const user = await context.db.query.user({ where: { email: args.email } }, ` { id password } `)
     if (!user) {
         throw new Error('No such user found')
     }
-  
-    // 2  compare the provided password with the one that 
+
+    // 2  compare the provided password with the one that
     // is stored in the database
     const valid = await bcrypt.compare(args.password, user.password)
     if (!valid) {
         throw new Error('Invalid password')
     }
-  
+
     const token = jwt.sign({ userId: user.id }, APP_SECRET)
-  
+
     // 3 returning token and user again
     return {
         token,
@@ -62,9 +62,35 @@ function post(parent, args, context, info) {
         info,
     )
 }
-  
+
+async function vote(parent, args, context, info) {
+    // 1
+    const userId = getUserId(context)
+
+    // 2
+    const linkExists = await context.db.exists.Vote({
+      user: { id: userId },
+      link: { id: args.linkId },
+    })
+    if (linkExists) {
+      throw new Error(`Already voted for link: ${args.linkId}`)
+    }
+
+    // 3
+    return context.db.mutation.createVote(
+      {
+        data: {
+          user: { connect: { id: userId } },
+          link: { connect: { id: args.linkId } },
+        },
+      },
+      info,
+    )
+}
+
 module.exports = {
     signup,
     login,
     post,
+    vote,
 }
